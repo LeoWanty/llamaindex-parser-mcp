2ced3999b228541ca1e23a7fa7cc35195e838b6b
leo.wanty <wanty.leo@gmail.com>
2025-09-12 16:02:13 +0200

    `chore(server): add BaseServer abstract class`
    Introduced `BaseServer` abstract class to define a standard structure for servers. This includes methods for retrieving tools and converting the server to a FastMCP server.

---
+++ src/mcp_llamaindex/servers/base.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from typing import Any, ClassVar
+
+from fastmcp import FastMCP
+from fastmcp.tools import Tool as FastMCPTool
+from pydantic import BaseModel, ConfigDict
+
+
+class BaseServer(BaseModel, ABC):
+    """A base server for all servers."""
+    server_name: str
+
+    model_config: ClassVar[ConfigDict] = ConfigDict(arbitrary_types_allowed=True)
+
+    @abstractmethod
+    def get_tools(self) -> list[FastMCPTool]: ...
+
+    @abstractmethod
+    def as_server(self) -> FastMCP[Any]:
+        """Convert the server to a FastMCP server."""
+
+        mcp: FastMCP[Any] = FastMCP[Any](name=self.server_name)
+
+        [mcp.add_tool(tool=tool) for tool in self.get_tools()]
+
+        return mcp
