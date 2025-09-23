19b19ca74aedbf75cf2d67d3571634764d53efcf
leo.wanty <wanty.leo@gmail.com>
2025-09-23 10:49:35 +0200

    `fix(logging): set utf-8 encoding for file handler`

--- src/mcp_llamaindex/logger.py
+++ src/mcp_llamaindex/logger.py
@@ -27,7 +27,7 @@ console_handler.setFormatter(formatter)
 root_logger.addHandler(console_handler)
 
 # File handler
-file_handler = logging.FileHandler(filename=SERVER_LOG_FILE, mode="a")
+file_handler = logging.FileHandler(filename=SERVER_LOG_FILE, mode="a", encoding="utf-8")
 file_handler.setLevel(log_level)
 file_handler.setFormatter(formatter)
 root_logger.addHandler(file_handler)
